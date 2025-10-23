56cf40eac52726374e6d251bfadc2f772b1ee704
matteovecchio04 <matteovex@gmail.com>
2025-10-23 19:15:22 +0200

    add bootstrap and css style, add a new button that generates 10 new emails and deletes the old ones

--- bonus/script.js
+++ bonus/script.js
@@ -2,19 +2,32 @@ const listEl = document.getElementById("list")
 
 const APIurl = "https://flynn.boolean.careers/exercises/api/random/mail"
 
-
-for (let i = 0; i < 10; i++) {
-    axios.get(APIurl)
-        .then(resp => {
-            console.log(resp.data.response);
-            listEl.innerHTML += `
+function randomEmails() {
+// decalre the function
+    listEl.innerHTML = ""
+    // clean string
+    for (let i = 0; i < 10; i++) {
+        axios.get(APIurl)
+            .then(resp => {
+                console.log(resp.data.response);
+                listEl.innerHTML += `
             <li>${resp.data.response}</li>
-            
             `
-        })
-        .catch(error => {
-            console.log(error);
-            
-            listEl.innerHTML = `Error`
-        })
+            })
+            .catch(error => {
+                console.log(error);
+                listEl.innerHTML = `Error`
+            })
+    }
+}
+
+randomEmails()
+// initialize the function
+
+const btnEl = document.getElementById("btn")
+// link the button
+btnEl.addEventListener("click", newEmails)
+// the event triggers when button is clicked and it starts the function randomEmails
+function newEmails() {
+    randomEmails()
 }
